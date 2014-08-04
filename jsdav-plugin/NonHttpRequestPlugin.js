909295c00a52178eb7ca0c2f15b874e7b07f37f7
Vidar Ramdal <vramdal@gmail.com>
2014-08-04 14:47:53 +0200

    NonHttpRequestPlugin

---
+++ jsdav-plugin/NonHttpRequestPlugin.js
@@ -0,0 +1,52 @@
+var jsDAV_ServerPlugin = require("../node_modules/jsDAV/lib/dav/plugin");
+var Path = require("path");
+var Url = require("url");
+var Util = require("../node_modules/jsDAV/lib/shared/util");
+var jsDAV_iFile = require("../node_modules/jsDAV/lib/DAV/interfaces/iFile");
+
+
+var mimeTool = require("mime");
+var Resource = require("../util/Resource");
+
+var jsDAV_NonHttpRequest_Plugin = module.exports = jsDAV_ServerPlugin.extend({
+	/**
+	 * Plugin name
+	 *
+	 * @var String
+	 */
+	name: "nonHttpRequest",
+
+	/**
+	 * reference to handler class
+	 *
+	 * @var jsDAV_Handler
+	 */
+	handler: null,
+
+	initialize: function (handler) {
+		this.handler = handler;
+		handler.addEventListener("beforeMethod", this.httpGetInterceptor.bind(this));
+	},
+
+	getResource: function(path, callback) {
+		this.handler.getNodeForPath(path, function(err, node) {
+			if (err) {
+				return callback(err);
+			}
+			if (!node.hasFeature(jsDAV_iFile)) {
+				return callback("Not a file");
+			}
+			node.getSize(function(err, size) {
+				if (err) {
+					return callback(err);
+				}
+				node.get(function(err, stringOrStream) {
+					if (err) {
+						return callback(err);
+					}
+					return callback(null, new Resource("/" + path, mimeTool.lookup(path), stringOrStream, size));
+				})
+			});
+		});
+	}
+});
